788a70f0ef8539c0cdb617501ae2d19319654d08
Vincent HURTEVENT <vincent.hurtevent@MacBook-Pro-de-Vincent.local>
2016-10-29 16:43:58 +0200

    Cacher le form create

--- forms/create.php
+++ forms/create.php
@@ -1,4 +1,5 @@
-<div id="signupbox" style="margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+
+<div id="signupbox" style="display:none;margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
     <div class="panel panel-info">
         <div class="panel-heading">
             <div class="panel-title">Créer votre compte Wahoo!</div>
