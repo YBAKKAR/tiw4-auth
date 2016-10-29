1b7cb2f9aeb1554db492138e8ea617d0a2d77083
Vincent HURTEVENT <vincent.hurtevent@MacBook-Pro-de-Vincent.local>
2016-10-29 13:43:58 +0200

    Commit initial

---
+++ forms/create.php
@@ -0,0 +1,34 @@
+<div id="signupbox" style="margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <div class="panel-title">Créer votre compte Wahoo!</div>
+        </div>
+        <div class="panel-body" >
+            <form id="signupform" class="form-horizontal" role="form">
+                <div class="form-group">
+                    <label for="username" class="col-md-3 control-label">Login</label>
+                    <div class="col-md-9">
+                        <input type="text" class="form-control" name="username" placeholder="login">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="email" class="col-md-3 control-label">Email</label>
+                    <div class="col-md-9">
+                        <input type="text" class="form-control" name="email" placeholder="email">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="password" class="col-md-3 control-label">Mot de Passe</label>
+                    <div class="col-md-9">
+                        <input type="password" class="form-control" name="passwd" placeholder="mot de passe">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-offset-3 col-md-9">
+                        <button id="btn-signup" type="submit" class="btn btn-info"><i class="icon-hand-right"></i>Valider</button>
+                    </div>
+                </div>
+            </form>
+         </div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Wahoo - Connexion</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	</head>
+	<body>
+
+		<?php
+		session_start();
+		if(isset($_SESSION['error']))
+		{
+		echo '<p>'.$_SESSION['error']['username'].'</p>';
+		echo '<p>'.$_SESSION['error']['email'].'</p>';
+		echo '<p>'.$_SESSION['error']['password'].'</p>';
+		unset($_SESSION['error']);
+		}
+		?>
+
+		<?php
+			session_start();
+			include('conf/db.inc');
+
+			if(isset($_POST['submit']))
+			{
+				$email = trim($_POST['email']);
+				$password = trim($_POST['password']);
+				$query = "SELECT * FROM user WHERE email='$email' AND password='$password'";
+				$result = mysqli_query($mysqli,$query)or die(mysqli_error());
+				$num_row = mysqli_num_rows($result);
+				$row=mysqli_fetch_array($result);
+
+				if( $num_row ==1 )
+				{
+					$_SESSION['user_name']=$row['username'];
+					header("Location: home.php");
+					exit;
+				}
+				else
+				{
+					include 'forms/login.php';
+				}
+
+			}
+		?>
+
+
+	</body>
+</html>
