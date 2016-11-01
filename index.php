ea03d53827342bf98f4bd5d84d5f03f32f3a402e
Vincent HURTEVENT <vincent.hurtevent@MacBook-Pro-de-Vincent.local>
2016-11-01 12:20:39 +0100

    Corr

--- index.php
+++ index.php
@@ -17,11 +17,17 @@
 
 			if(isset($_SESSION['error']))
 			{
-				echo "<div id=\"error-message\" style=\"margin-top:50px;\" class=\"mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2\">";
+				echo "<div id=\"error-message\" style=\"margin-top:50px;\" class=\"mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2\">
+					  	<div class=\"panel panel-info\">
+							<div class=\"panel-heading\">
+								<div class=\"panel-title\">Erreur</div>";
 				echo "<p class=\"text-danger\">".$_SESSION['error']['username']."</p>";
 				echo "<p class=\"text-danger\">".$_SESSION['error']['email']."</p>";
 				echo "<p class=\"text-danger\">".$_SESSION['error']['password']."</p>";
-				echo "</div>";
+				echo "			</div>
+							</div>
+						</div>
+					  </div>";
 				unset($_SESSION['error']);
 			}
 
