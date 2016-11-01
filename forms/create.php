4dc352cb54bfac833eab5b08249209d635fe4142
Vincent HURTEVENT <vincent.hurtevent@MacBook-Pro-de-Vincent.local>
2016-11-01 11:45:59 +0100

    corr

--- forms/create.php
+++ forms/create.php
@@ -27,7 +27,7 @@
                 </div>
                 <div class="form-group">
                     <div class="col-md-offset-3 col-md-9">
-                        <button id="btn-signup" type="submit" class="btn btn-info"><i class="icon-hand-right"></i>Valider</button>
+                        <input id="btn-signup" type="submit" class="btn btn-info" name="submit" type="submit" value="Submit"/>
                     </div>
                 </div>
             </form>
