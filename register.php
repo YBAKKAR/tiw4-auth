9484c9f661cbaae5347632a17ca5b1eb4797ee98
Vincent HURTEVENT <vincent.hurtevent@MacBook-Pro-de-Vincent.local>
2016-11-01 12:27:42 +0100

    Corr

--- register.php
+++ register.php
@@ -1,73 +1,91 @@
-<?php
 
-	session_start();
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-	include('conf/db.inc');
+	<?php
+		echo file_get_contents("header.php");
+	?>
 
-echo $_POST['username'];
-echo $_POST['email'];
-echo $_POST['password'];
+	<body>
 
+		<div class="container">    
+			<div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
+				<div class="panel panel-info">
+					<div class="panel-heading">
+						<div class="panel-title">Enregistrement</div>
+					</div>     
+					<div style="padding-top:30px" class="panel-body" >
 
-	if(isset($_POST['submit']))
-	{
-		if($_POST['username'] == '')
-		{
-			$_SESSION['error']['username'] = "Login manquant";
-		}
-		if($_POST['email'] == '')
-		{
-			$_SESSION['error']['email'] = "E-mail manquant";
-		} else {
-			if(preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9._-]+)+$/", $_POST['email']))
-			{
-				$email= $_POST['email'];
-				$sql1 = "SELECT * FROM user WHERE email = '$email'";
-				$result1 = mysqli_query($mysqli,$sql1) or die(mysqli_error());
-				if (mysqli_num_rows($result1) > 0)
-				{
-					$_SESSION['error']['email'] = "Un compte existe déjà avec cet e-mail.";
-				}
-			} else {
-				$_SESSION['error']['email'] = "E-mail non valide";
-			}
-		}
-		if($_POST['password'] == '')
-		{
-			$_SESSION['error']['password'] = "Mot de passe manquant";
-		}
+						<?php
 
-		if(isset($_SESSION['error']))
-		{
-			header("Location: index.php");
-			exit;
-		} else {
-			$username = $_POST['username'];
-			$email = $_POST['email'];
-			$password = $_POST['password'];
+							session_start();
 
-			$sql2 = "INSERT INTO user (username, email, password) VALUES ('$username', '$email', '$password')";
-			$result2 = mysqli_query($mysqli,$sql2) or die(mysqli_error());
+							include('conf/db.inc');
 
-			if($result2)
-			{
-				$to = $email;
-				$subject = "Confirmation de création de votre compte Wahoo!";
-				$header = "Wahoo! - Votre compte";
-				$message = "Vos identifiants Wahoo! : \n";
-				$message .= "    Login: $username\n";
-				$message .= "    Mot de passe: $password\n";
-				$message .= "A bientôt !";
+							if(isset($_POST['submit']))
+							{
+								if($_POST['username'] == '')
+								{
+									$_SESSION['error']['username'] = "Login manquant";
+								}
+								if($_POST['email'] == '')
+								{
+									$_SESSION['error']['email'] = "E-mail manquant";
+								} else {
+									if(preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9._-]+)+$/", $_POST['email']))
+									{
+										$email= $_POST['email'];
+										$sql1 = "SELECT * FROM user WHERE email = '$email'";
+										$result1 = mysqli_query($mysqli,$sql1) or die(mysqli_error());
+										if (mysqli_num_rows($result1) > 0)
+										{
+											$_SESSION['error']['email'] = "Un compte existe déjà avec cet e-mail.";
+										}
+									} else {
+										$_SESSION['error']['email'] = "E-mail non valide";
+									}
+								}
+								if($_POST['password'] == '')
+								{
+									$_SESSION['error']['password'] = "Mot de passe manquant";
+								}
 
-				$sentmail = mail($to,$subject,$message,$header);
+								if(isset($_SESSION['error']))
+								{
+									header("Location: index.php");
+									exit;
+								} else {
+									$username = $_POST['username'];
+									$email = $_POST['email'];
+									$password = $_POST['password'];
 
-				if($sentmail)
-				{
-				echo "Un mail de confirmation vous a été envoyé à l'adresse $email.";
-				} else {
-					echo "Erreur lors de l'envoi du mail de confirmation.";
-				}
-			}
-		}
-	}
-?>
+									$sql2 = "INSERT INTO user (username, email, password) VALUES ('$username', '$email', '$password')";
+									$result2 = mysqli_query($mysqli,$sql2) or die(mysqli_error());
+
+									if($result2)
+									{
+										$to = $email;
+										$subject = "Confirmation de création de votre compte Wahoo!";
+										$header = "Wahoo! - Votre compte";
+										$message = "Vos identifiants Wahoo! : \n";
+										$message .= "    Login: $username\n";
+										$message .= "    Mot de passe: $password\n";
+										$message .= "A bientôt !";
+
+										$sentmail = mail($to,$subject,$message,$header);
+
+										if($sentmail)
+										{
+										echo "<p class="bg-success">Un mail de confirmation vous a été envoyé à l'adresse $email.</p>";
+										} else {
+											echo "<p class="bg-danger">Erreur lors de l'envoi du mail de confirmation.</p>";
+										}
+									}
+								}
+							}
+						?>
+
+			</div>
+		</div>
+	</div>
+</div>
